f953ed0033655c2e98637fe8578687b645bdc4c5
ftovar8 <felix.godo@gmail.com>
2023-06-13 08:44:18 -0400

    clean code

--- __sources/models.ts/bundles/packagers/compilers/collection.ts
+++
@@ -1,9 +0,0 @@
-import {Collection, CollectionSpecs} from "@beyond-js/plm/core";
-import {PackagerCompiler} from "./item";
-
-export /*bundle*/
-class PackagerCompilers extends Collection {
-    constructor(specs: CollectionSpecs) {
-        super('packagers-compilers', PackagerCompiler, specs);
-    }
-}
--- __sources/models.ts/distributions/interfaces/ports.ts
+++
@@ -1,5 +0,0 @@
-export interface PortsSpecs {
-    bundles?: number,
-    http?: number,
-    inspect?: number
-}
